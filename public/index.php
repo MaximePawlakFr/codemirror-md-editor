df1b209c2f69a524d649402e132eb038517775d8
Semere <BINI>
2016-05-17 10:54:32 +0200

    First pages commit

---
+++ public/index.php
@@ -0,0 +1,48 @@
+<?php
+
+
+$comment = null;
+
+if($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['preview-form-comment'])) {
+	$comment = $_POST['preview-form-comment'];
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>React all the things !</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="codemirror/lib/codemirror.css">
+	<link rel="stylesheet" type="text/css" href="css/semantic.min.css">
+</head>
+<body>
+	<h1>Codemirror</h1>
+	<div class="ui grid">
+		<div class="two column row">
+			<div class="ui eight wide column segment">
+				<form id="preview-form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+					<textarea class="codemirror-textarea" name="preview-form-comment" id="preview-form-comment"><?php echo $comment; ?></textarea>
+					<br>
+					<input type="submit" name="preview-form-submit" id="preview-form-submit" value="View">
+				</form>
+			</div>
+			<div class="ui eight wide column segment">
+				<div id="preview-comment"><?php echo $comment; ?></div>
+			</div>
+		</div>
+	</div>
+
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script src="codemirror/lib/codemirror.js"></script>
+	<script src="js/semantic.min.js"></script>
+	<script src="js/marked.js"></script>
+	<script src="js/app1.js"></script>
+	<script type="text/javascript">
+
+	</script>
+</body>
+</html>
---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+
+//index.html
+
+document.getElementById('result').innerHTML =
+marked(localStorage.getItem('text'));
+
+
+function saveEditor(){
+	var editorValue = document.getElementById("comment").value;
+	localStorage.setItem('text', editorValue);
+}
+
+var previewLoad = setInterval(function() { saveEditor() },1000);
+
+var editor;
+function load(){
+
+	editor = CodeMirror.fromTextArea(document.getElementById("comment"), {
+		mode:  "javascript",
+		theme: 'icecoder',
+		styleActiveLine: true,
+		lineNumbers: true,
+	});
+
+	var storeValue = localStorage.getItem('text');
+	if (storeValue) {
+		document.getElementById('comment').value = storeValue;
+	};
+}
+
+load();
+
+function submit_html(){
+
+	editor.save();
+	var code = document.getElementById("comment").value;
+	document.getElementById('result').innerHTML =
+	marked(code);
+}
+
+var preview = setInterval(function() { submit_html() },1000);
